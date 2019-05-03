7a79c21219eb2bc0d16135665dbbdb1d49ad0109
WilliamsSantos <Williamssantos1996@outlook.com>
2019-05-03 00:02:29 -0300

    Realizadas Validacoes

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,9 @@
 class Order < ApplicationRecord
   belongs_to :restaurant
   has_many :order_products
+  enum status: { waiting: 0, delivered: 1 }
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :total_value, presence: true
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,11 @@ class Restaurant < ApplicationRecord
   has_many :product_categories
   has_many :orders
   has_many :reviews
+  enum status: { closed: 0, open: 1 }
+
+  validates_associated :category
+  validates :name, :status, :delivery_tax, :city, :street, presence: true
+  
+
+
 end
